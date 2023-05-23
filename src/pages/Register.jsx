877546ba935420b3dfb3ce5a31abf6d425cff8e1
Wilson Chen <wilsonchen901@gmail.com>
2023-05-23 13:06:19 -0400

    start register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Register = () => {
+    return (
+        <div className="form-container">
+        <div className="form-wrapper">
+            <span className="logo">Star Rail Messenger</span>
+            <span className="title">Register</span>
+            <form>
+                <input type="text" placeholder="Display Name"/>
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+                <input type="file" placeholder="Avatar"/>
+                <button>Sign Up</button>
+            </form>
+            <p>Already have an account? Login</p>
+        </div>
+    </div>
+    )
+};
+
+export default Register;
