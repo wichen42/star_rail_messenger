d7d6c56fcb8800f5c3c73a857250afb5a30ce63a
Wilson Chen <wilsonchen901@gmail.com>
2023-05-24 18:03:27 -0400

    finsih basic message stlying

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,14 +3,15 @@ import PomPom from "../assets/pompom.jpg";
 
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
         <div className="message-info">
             <img src={PomPom} alt="" />
             <span>just now</span>
         </div>
         <div className="message-content">
+            <span>User</span>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque magnam, aliquam, ut expedita repudiandae optio magni voluptatem impedit temporibus a ipsa quidem rerum fugiat aut. Suscipit blanditiis fuga magnam laboriosam.</p>
-            {/* <img src={PomPom} alt="" /> */}
+            <img src={PomPom} alt="" />
         </div>
     </div>
   )
