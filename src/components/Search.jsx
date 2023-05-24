25b9e9d18dd61f7fba57e3f6241600d9a34663b8
Wilson Chen <wilsonchen901@gmail.com>
2023-05-24 15:12:59 -0400

    work on sidebar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className='search-form'>
+        <input type="text" placeholder='Search for User'/>
+      </div>
+      <div className='user-chat'>
+        <img src="https://images.pexels.com/photos/16881619/pexels-photo-16881619/free-photo-of-nunca-tan-lejos-de-la-realidad.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+        <div className='user-chat-info'>
+          <span>User</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
