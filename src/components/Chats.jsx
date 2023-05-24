9409012af1232195f2aac92d9fcbac2d993f2812
Wilson Chen <wilsonchen901@gmail.com>
2023-05-24 15:30:51 -0400

    style chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -3,7 +3,37 @@ import React from 'react'
 const Chats = () => {
   return (
     <div className='chats'>
-      Chats
+      <div className='user-chat'>
+        <img src="https://images.pexels.com/photos/16881619/pexels-photo-16881619/free-photo-of-nunca-tan-lejos-de-la-realidad.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+        <div className='user-chat-info'>
+          <span>User</span>
+          <p>Bio Text</p>
+        </div>
+      </div>
+
+      <div className='user-chat'>
+        <img src="https://images.pexels.com/photos/16881619/pexels-photo-16881619/free-photo-of-nunca-tan-lejos-de-la-realidad.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+        <div className='user-chat-info'>
+          <span>User</span>
+          <p>Bio Text</p>
+        </div>
+      </div>
+
+      <div className='user-chat'>
+        <img src="https://images.pexels.com/photos/16881619/pexels-photo-16881619/free-photo-of-nunca-tan-lejos-de-la-realidad.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+        <div className='user-chat-info'>
+          <span>User</span>
+          <p>Bio Text</p>
+        </div>
+      </div>
+
+      <div className='user-chat'>
+        <img src="https://images.pexels.com/photos/16881619/pexels-photo-16881619/free-photo-of-nunca-tan-lejos-de-la-realidad.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="" />
+        <div className='user-chat-info'>
+          <span>User</span>
+          <p>Bio Text</p>
+        </div>
+      </div>
     </div>
   )
 }
