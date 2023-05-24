36c71c152fa798ef287d9348bdd5578e69a627cb
Wilson Chen <wilsonchen901@gmail.com>
2023-05-24 17:45:31 -0400

    styling message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import PomPom from "../assets/pompom.jpg";
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message">
+        <div className="message-info">
+            <img src={PomPom} alt="" />
+            <span>just now</span>
+        </div>
+        <div className="message-content">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque magnam, aliquam, ut expedita repudiandae optio magni voluptatem impedit temporibus a ipsa quidem rerum fugiat aut. Suscipit blanditiis fuga magnam laboriosam.</p>
+            {/* <img src={PomPom} alt="" /> */}
+        </div>
+    </div>
   )
 }
 
