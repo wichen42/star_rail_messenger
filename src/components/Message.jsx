b8cd76f5855d3f26ab14c7c2eec603098537c3be
Wilson Chen <wilsonchen901@gmail.com>
2023-05-31 18:00:41 -0400

    convert message timestamp to date

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,24 +1,46 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef, useState} from 'react'
 import { AuthContext } from '../context/AuthContext';
 import { ChatContext } from '../context/ChatContext';
 
 const Message = ({message}) => {
   const {currentUser} = useContext(AuthContext);
   const {data} = useContext(ChatContext);
+  const [date, setDate] = useState("");
   const ref = useRef();
+
 
   useEffect(() => {
     ref.current?.scrollIntoView({behavior: "smooth"});
+
+    const getDate = () => {
+      const fireBaseTime = new Date(
+        message.date.seconds * 1000 + message.date.nanoseconds / 1000000
+      );
+  
+      const unsub = () => {
+        setDate(fireBaseTime.toLocaleDateString());
+      };
+
+      return () => {
+        unsub();
+      };
+    };
+
+    getDate();
+    
   }, [message]);
+
 
   return (
     <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
         <div className="message-info">
             <img src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL} alt="" />
-            <span>{}</span>
+            {/* TODO: display date time */}
+            <span>{date}</span>
         </div>
         <div className="message-content">
-            <span>User</span>
+          {/* TODO: convert senderId into displayname */}
+            <span>{message.senderId}</span>
             <p>{message.text}</p>
             {message.img && <img src={message.img} alt="" />}
         </div>
