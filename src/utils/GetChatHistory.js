e0bf186dcdfb57519753dc9c5dca8e76bb2e0dc7
Wilson Chen <wilsonchen901@gmail.com>
2023-06-21 22:19:05 -0400

    add todos for chat history

--- src/utils/GetChatHistory.js
+++ src/utils/GetChatHistory.js
@@ -1,13 +1,17 @@
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
 
+// TODO: 1. PROCESS CHAT HISTORY
+//         a. CONVERT SENDERID INTO DISPLAYNAME
+//         b. FORMAT HISTORY TO DISPLAYNAME:TEXT && CHATBOT:TEXT
+//         c. DELETE OLDEST CHAT HISTORY IF PASS TOKEN LIMIT
+
 function useGetChatHistory () {
 
     async function getChatHistory (chatId) {
         const docSnap = await getDoc(doc(db, "chats", chatId));
         if (docSnap.exists()) {
-            const chatHistory = docSnap.data();
-            console.log(chatHistory);
+            return docSnap.data();
         } else {
             console.log("Issue with retrieving chat history...");
         }
