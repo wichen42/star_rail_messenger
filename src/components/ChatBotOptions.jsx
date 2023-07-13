4b6a16a714f366a22aadd5708c326d268ef18b11
Wilson Chen <wilsonchen901@gmail.com>
2023-07-13 14:35:32 -0400

    chatbot options component and styling

--- src/components/ChatBotOptions.jsx
+++ src/components/ChatBotOptions.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react";
 
 const ChatBotOptions = () => {
+    const [showOptions, setShowOptions] = useState(false);
+
     const handleMore = (e) => {
         e.preventDefault();
-        console.log("clicked");
+        setShowOptions(!showOptions);
+    };
+
+    const handleOption = (e) => {
+        e.preventDefault();
+        setShowOptions(!showOptions);
     };
 
     return (
-    <div className="chatbot-options">
-        <span className="material-symbols-outlined" onClick={handleMore}>more_horiz</span>
+    <div className="chatbot-options-container">
+        {!showOptions && <span className="material-symbols-outlined" onClick={handleMore}>more_horiz</span>}
+        {showOptions && (
+            <div className="chatbot-options">
+                <p onClick={handleOption}>Who are you?</p>
+                <p onClick={handleOption}>What is the inspiration behind this app?</p>
+                <p onClick={handleOption}>Tell me a joke!</p>
+            </div>
+        )}
     </div>
     )
 }
